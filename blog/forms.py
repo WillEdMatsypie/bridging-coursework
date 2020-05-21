5b2d04c3f5c97f57da685dba2fb7ec8a098fa497
Zenith <willedmats@gmail.com>
2020-05-21 21:04:13 +0100

    Made forms on the website for posting and commenting generally better

--- blog/forms.py
+++ blog/forms.py
@@ -7,6 +7,18 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'subtitle', 'text',)
+        widgets = {
+            'title': forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Post Title',
+            }),
+            'subtitle': forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Post Caption'}),
+            'text': forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Post Text...'})
+         }
 
 
 class CommentForm(forms.ModelForm):
@@ -14,3 +26,12 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('author', 'text',)
+        widgets = {
+            'author': forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Your Name',
+            }),
+            'text': forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Comment...'})
+         }
