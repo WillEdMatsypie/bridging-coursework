8e4caf58f4ef6ad4536868ccebaf47e052fff4b9
Zenith <willedmats@gmail.com>
2020-05-27 20:43:50 +0100

    Added homepage to website with some basic tests

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.urls import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+
+from home.views import home  
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_view(self):
+        found = resolve('/')  
+        self.assertEqual(found.func, home)
+    
+    def test_home_returns_correct_html(self):
+        request = HttpRequest()  
+        response = home(request)  
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.strip().startswith('<html>'))  
+        self.assertIn('<title>Zenith\'s Blog</title>', html)  
+        self.assertIn('<h1>Zenith</h1>', html)
+        self.assertTrue(html.endswith('</html>'))  
