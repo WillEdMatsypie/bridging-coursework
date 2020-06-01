c0769319cec5667832437968faf332c94c7a61a3
Zenith <willedmats@gmail.com>
2020-06-01 15:43:00 +0100

    Added URLs for experience

--- cv/urls.py
+++ cv/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('education/<int:pk>/remove/', views.education_remove, name='education_remove'),
     path('skill/new/', views.skill_new, name='skill_new'),
     path('skill/<int:pk>/remove/', views.skill_remove, name='skill_remove'),
+    path('experience/new/', views.experience_new, name='experience_new'),
+    path('experience/<int:pk>/remove/', views.experience_remove, name='experience_remove'),
 ]
